17f589d36fdc1a29962db34067f8b8c65bee52a4
edmundlemon <edmundngxhunyan@gmail.com>
2025-05-10 17:59:18 +0800

    Change to verify by user ID

--- Backend/database/migrations/2025_04_19_042207_create_admins_table.php
+++ Backend/database/migrations/2025_04_19_042207_create_admins_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('admins', function (Blueprint $table) {
-            $table->id();
+            $table->id()->startingValue(1001);
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
