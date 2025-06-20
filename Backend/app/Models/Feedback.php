fe0ef75aff85fc9e5458440eb28bb5440f3982e7
edmundlemon <edmundngxhunyan@gmail.com>
2025-06-20 23:37:55 +0800

    Fixed Event Feedback issue

--- Backend/app/Models/Feedback.php
+++ Backend/app/Models/Feedback.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Feedback extends Model
 {
     use HasFactory;
+
+   protected $fillable = [
+        'user_id',
+        'event_id',
+        'feedback_remarks',
+        'rating',
+        'feedback_date',
+    ];
 }
